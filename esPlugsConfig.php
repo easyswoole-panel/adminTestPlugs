e9a1dda36b9afc9ef2558ade5543724259b69fbb
Siam <59419979@qq.com>
2020-12-01 23:08:46 +0800

    first

---
+++ esPlugsConfig.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'name'      => '测试插件',
+    'des'       => '介绍',
+    'namespace' => "\\Siam\\TestPlugs\\",
+    'version'   => '1.0'
+];
---
+++ src/database/install_1.0.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 1.0安装脚本
+ * User: Siam
+ * Date: 2020/12/1 0001
+ * Time: 22:54
+ */
+
+// 创建表结构
+// 更改表结构
+// 添加数据
+// 移动view文件 都可以
+\EasySwoole\Utility\File::createFile(EASYSWOOLE_ROOT."/public/nepadmin/views/testPlugs/index.html", "siam你好 自动安装");
